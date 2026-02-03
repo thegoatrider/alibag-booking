0bdec3bdf214e86214775e413476789d06a8d7f6
Ananya Pandey <goatrider1403@gmail.com>
2026-02-03 16:53:08 +0530

    update

--- app/dashboard/influencer/properties/page.tsx
+++ app/dashboard/influencer/properties/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = "force-dynamic";
-
 "use client";
+
+export const dynamic = "force-dynamic";
 
 import { Suspense } from "react";
 import InfluencerPropertiesInner from "./properties-inner";
